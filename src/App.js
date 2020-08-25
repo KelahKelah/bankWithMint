c22c567406587465282b8008ff2f740574dd7a7c
KelahKelah <kelechiekebor@gmail.com>
2020-08-25 12:29:27 +0100

    design sidebar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Header from './components/header/header'
+import Header from './components/header/header';
+import Siderbar from './components/sidebar/sidebar';
+import Merchant from './pages/merchant/merchant';
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      {/* <Header /> */}
+      {/* <Siderbar /> */}
+      <Merchant />
     </div>
   );
 }
