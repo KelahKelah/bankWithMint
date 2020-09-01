2e917a9408e729bd2befb001d365fd9b3a55bda7
KelahKelah <kelechiekebor@gmail.com>
2020-09-01 12:05:56 +0100

    modified chart module

--- src/components/charts/mainChart.js
+++ src/components/charts/mainChart.js
@@ -32,7 +32,7 @@ const MainChart = () => {
    }, [])
 
    const lineChart = (
-       <div style = {{width: '700px', height: '500px' , }}>
+       <div style = {{width: '350px', height: '350px' , }}>
             <Chart data={data} axes={axes} />
        </div>
    )
